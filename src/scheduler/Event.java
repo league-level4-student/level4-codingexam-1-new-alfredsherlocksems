b5e4bdff26e4a8b29a7a1c79d6024f326c1e1e1f
student <student@ip-172-31-57-163.us-west-2.compute.internal>
2022-11-07 18:29:48 -0800

    FINISHED TEST NOW ON LEVEL 5

--- src/scheduler/Event.java
+++ src/scheduler/Event.java
@@ -3,10 +3,13 @@ package scheduler;
 public class Event {
 	String day;
 	int time;
-	public Event(int time, String day) {
+	String name;
+	public Event(int time, String day, String name) {
 		this.day = day;
 		this.time = time;
+		this.name = name;
 	}
+	
 	public String getDay() {
 		return day;
 	}
@@ -14,10 +17,19 @@ public class Event {
 	public int getTime() {
 		return time;
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
 	public void setDay(String thing) {
 		day = thing;
 	}
 	public void setTime(int value) {
 		time = value;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
 	}
 }
--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -47,8 +47,9 @@ public class Scheduler {
 	
 	
     public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
     	while (forever) {
-    		Scanner scanner = new Scanner(System.in);
+    		
     		System.out.println("What would you like to do?");
     		String input = scanner.nextLine();
     		if(input.equals("add")) {
@@ -56,22 +57,36 @@ public class Scheduler {
     			addEvent(scanner);
     		}
     		else if(input.equals("view") ) {
-    			
+    			System.out.println("Current events: ");
+    			System.out.println();
+    			for (int i = 0; i < days.length; i++) {
+    				if (days[i].events.size() == 0) {
+    					System.out.println("You have no events on " + days[i].name().toLowerCase());
+    				}
+    				else {
+    					System.out.println("On " + days[i].name().toLowerCase() + " your event[s] are: ");
+    				}
+					Node<Event> node = days[i].events.getHead();
+					while(node != null) {
+						System.out.println("     You have " + node.getValue().getName() + " at " + node.getValue().getTime());
+						node = node.getNext();
+					}
+				}
     		}
     		else if(input.equals("remove")) {
-    			
+    			removeEvent(scanner);
     		}
     		else {
-    			scanner.close();
-    			continue;
+    			System.out.println("invalid input");
     		}
-    	
-    		scanner.close();
+    		
     	}
+    	scanner.close();
     }
     static //I NEED TO ADD A BUBBLE SORT TO MAKE THE EVENTS CHRONOLOGICAl
     void addEvent(Scanner scanner){
     	SchedulingConflictException exception = new SchedulingConflictException();
+    	boolean dayFound = false;
     	boolean found = false;
     	int time = 0;
     	while (!found) {
@@ -79,9 +94,13 @@ public class Scheduler {
 			String day = scanner.nextLine();
 			for (int i = 0; i < days.length; i++) {
 				if (days[i].name().equals(day)) {
+					dayFound = true;
 					System.out.println("So you want the event on " + day + "! What time do you want it to be? (format = 1400)");
-					time = scanner.nextInt();
-					Event event = new Event(time, day);
+					String temp = scanner.nextLine();
+					time = Integer.parseInt(temp);
+					System.out.println("What would you like the event name to be?");
+					String name = scanner.nextLine();
+					Event event = new Event(time, day, name);
 					Node<Event> node = new Node<Event>(event);
 				
 					if (days[i].events.getHead() != null) {
@@ -96,31 +115,76 @@ public class Scheduler {
 							}
 							checker = checker.getNext();
 						}
-						Node<Event> list = days[i].events.getTail();
-						Node<Event> prev = days[i].events.getTail();
-						prev = prev.getPrev();
-						days[i].events.setTail(node);
-						prev.setNext(list);
-						list.setNext(node);
-						for (int o = 0; o < days.length; o++) {	
-							for (int j = 0; j < days.length; j++) {
-								//THIS SHOULD BE THE BUBBLE SORT	
-							}
-						}
-					}
-					else {
-						Event event = new Event(time, day);
-						Node<Event> node = new Node<Event>(event);
 						
-						days[i].events.setHead(node);
+						days[i].events.add(event);
+						for (int j = 0; j < days[i].events.size(); j++) {	
+							
+						}
+						found = true;
+						System.out.println("added");
+						return;
+				}
 					}
-					found = true;
-					return;
+					
 				}
 			}
-    	}
+    	
     
+    public static void removeEvent(Scanner scanner) {
+    	boolean found = false;
+    	int num = 0;
+    	int time = 0;
+    	while(!found) {
+	    	System.out.println("What day do you want to remove the event?");
+	    	String day = scanner.nextLine();
+	    	for (int i = 0; i < days.length; i++) {
+				if (days[i].name().equals(day)) {
+					num = i;
+					found = true;
+				}
+			}
+	    	if (!found) {
+	    		System.out.println("Invalid input");
+	    	}
+    	}
+    	System.out.println("What is the time of the event?");
+    	time = scanner.nextInt();
+    	
+    	
+    	boolean foundNode = false;
+    	while (!foundNode) {
+    		Node<Event> node = days[num].events.getHead();
+	    	while (node != null && !foundNode) {
+	    		if (node.getValue().getTime() == time) {
+	    			Node <Event> prev = node;
+	    			Node<Event> next = node;
+	    			if (node.getPrev() != null && node.getNext()!= null) {
+	    				prev = node.getPrev();
+	    				next = node.getNext();
+	    				prev.setNext(next);
+	    				next.setPrev(prev);
+	    			}
+	    			else if (node == days[num].events.getHead()) {
+	    				days[num].events.setHead(null);
+	    			}
+	    			else {
+	    				days[num].events.setTail(null);
+	    			}
+	    			
+	    			foundNode = true;
+	    			continue;
+	    		}
+	    		node = node.getNext();
+	    	}
+	    	if (!foundNode) {
+	    		System.out.println("Invalid input");
+	    	}
+    	}
+    	System.out.println("removed");
+	    	
     }
+    
+}
     
     
 
